ed2135816772bee9b0dc8dcb30836d5559fee0c1
ea <elliea915@gmail.com>
2017-01-12 22:20:59 -0800

    add route

--- app/router.js
+++ app/router.js
@@ -5,6 +5,9 @@ const Router = Ember.Router.extend({
   location: config.locationType
 });
 
+Router.map(function() {
+  this.route('question', {path: '/question/:question_id'});
+});
 
 
 export default Router;
